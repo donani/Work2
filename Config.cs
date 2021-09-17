b1a0b04f1b9a493cbc194bc860d0d1a453885457
donani <90883437+donani@users.noreply.github.com>
2021-09-17 15:53:36 +0900

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Configuration;
+
+namespace POC.Console.Sender
+{
+    class Config
+    {
+        public static string GetConfig(string configKey)
+        {
+            string sResult = string.Empty;
+            sResult = System.Configuration.ConfigurationSettings.AppSettings[configKey];
+            return sResult;
+        }
+    }
+}
